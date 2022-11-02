d1763274d9f991ef2f06ffc1834a6aa59bb05c58
David Armeni <david.armeni1993@gmail.com>
2022-11-02 14:58:39 +0200

    commit part2 course info 2.4 (step 9)

--- part2/courseInfo/src/App.js
+++ part2/courseInfo/src/App.js
@@ -1,27 +1,55 @@
 function App() {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const course = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      <Course course={course[0]} />
+      <Course course={course[1]} />
+    </div>
+  );
 }
 
 const Course = (props) => {
@@ -46,13 +74,15 @@ const Header = (props) => {
 const Content = (props) => {
   const partsArr = props.course;
   console.log(partsArr);
-  return (
-    <div>
-      <Part part={partsArr[0].name} exercises={partsArr[0].exercises} />
-      <Part part={partsArr[1].name} exercises={partsArr[1].exercises} />
-      <Part part={partsArr[2].name} exercises={partsArr[2].exercises} />
-    </div>
-  );
+  return partsArr.map((part) => {
+    return <Part part={part.name} exercises={part.exercises} />;
+  });
+  // <div>
+  //   <Part part={partsArr[0].name} exercises={partsArr[0].exercises} />
+  //   <Part part={partsArr[1].name} exercises={partsArr[1].exercises} />
+  //   <Part part={partsArr[2].name} exercises={partsArr[2].exercises} />
+  //   <Part part={partsArr[3].name} exercises={partsArr[3].exercises} />
+  // </div>
 };
 
 const Part = (props) => {
