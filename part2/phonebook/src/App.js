a4ed3f840bcba421df062432bff6d4e06ffe7fcb
David93Code <david.armeni1993@gmail.com>
2022-11-14 14:28:57 +0200

    commit phonebook 2.11 step6 axios hook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PersonForm from "./components/personform";
 import Persons from "./components/persons";
 import Filter from "./components/filter";
+import axios from "axios";
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040 - 1234567", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
 
   const [newNumber, setNewNumber] = useState("");
@@ -17,6 +14,17 @@ const App = () => {
   console.log(search);
 
   console.log(persons);
+
+  const hook = () => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled');
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(hook, []);
 
   return (
     <div>
